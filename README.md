238a0bf291fc4cd71193a35836d8aa4e52fa294c
james <james@bubbleye.com>
2023-02-14 20:13:51 +0800

    Few updates to notes

--- README.md
+++ README.md
@@ -2,25 +2,31 @@
 
 Notes and how to for capturing HTTP traffic from mobile devices for security analysis and advertising ops.
 
-## Setup
+## Setup Docs
 
 - [Waydroid](https://docs.waydro.id/usage/install-on-desktops): Android Emulator for Linux
 - [Waydroid Transparent Proxy](https://docs.mitmproxy.org/stable/howto-transparent/)
 - [Waydroid Script](https://github.com/casualsnek/waydroid_script) Play services for using playstore and Google Play services
   - currently having issues: [Issue](https://github.com/casualsnek/waydroid_script/issues/68)
 
-## Running after Setup
+## Startup after Installation (only working for browsers)
 
-1. Start Waydroid: `waydroid show-full-ui` and check internet
+1. Ensure iptables is working. I was able to solve this by explicitly add `/lib/x86_64-linux-gnu/xtables` to `/etc/ld.so.conf.d/x86_64-linux-gnu.conf` and rebooting. This was reported working for Debian 11 and Ubuntu 20+
+2. Run `./proxysetup.sh 8080 -s` for waydroid. Use `-l` if mitm for other device
+3. Start Waydroid service: `waydroid session start`
+4. Start Waydroid UI: `waydroid show-full-ui` and check internet
    1. If internet can't connect try `sudo waydroid shell` and check `ip link` and `ip addr` to see if firewall blocking.
    2. If needed disable firewall by `sudo ufw disable` or open udp67? More info at: [ArchWiki Waydroid Networking](https://wiki.archlinux.org/title/Waydroid#Network)
-2. Ensure iptables is working. I was able to solve this by explicitly add `/lib/x86_64-linux-gnu/xtables` to `/etc/ld.so.conf.d/x86_64-linux-gnu.conf` and rebooting. This was reported working for Debian 11 and Ubuntu 20+
-3. Run `./proxysetup.sh 8080 -l`. Skip `-l` if mitm for other device
-   1. Install certs (first time per device)
+   3. Install certs (first time per device)
       1. On target device, navigate to `mitm.it`
       2. follow instructions on mitm.it after downloading
          1. eg: `mv mitmproxy-ca-cert.pem /usr/local/share/ca-certificates/mitmproxy.crt`
       3. NOTE: Firefox has SEPARTE certs from OS certs
-4. 
 
 NOTE: `proxysetup.sh` runs `sudo iptables -t nat -F` at end to clear out iptables, which can cause your connection to be blocked. But be warned, this will clear all custom iptables on your nat table.
+
+## Startup for Apps
+
+Let's try using (mitmproxy wireguard mode)[https://mitmproxy.org/posts/wireguard-mode/].
+
+Run by `mitmweb --mode wireguard`
--- proxysetup.sh
+++ proxysetup.sh
@@ -18,11 +18,30 @@ fi
 
 
 
+# MITM virtualenv 
+source ~/mitmproxy/.virtualenv/bin/activate
+
+
 # Set forwarding and redirects
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo sysctl -w net.ipv6.conf.all.forwarding=1
 sudo sysctl -w net.ipv4.conf.all.send_redirects=0
 
+
+#export http_proxy='http://127.0.0.1:8080'
+#export https_proxy='https://127.0.0.1:8080'
+
+#for those trying to create a mitmproxy to inspect traffic from android, after a long time I got it working without iptables stuff:
+#
+#1. set the localhost port 8080 as proxy for everything in your waydroid:
+#adb shell settings put global http_proxy 127.0.0.1:8080
+#
+#2. because the localhost from waydroid is different from the localhost of the host machine, you need to forward the ports (like "ssh -L"):
+#adb reverse tcp:8080 tcp:8080
+#
+
+#sudo waydroid shell settings put global http_proxy '127.0.0.1:8080'
+#sudo waydroid shell reverse tcp:8080 tcp:8080
 
 if [ "$local" = "-l" ]; # check if is local
 then
@@ -39,6 +58,7 @@ then
     sudo ip6tables -t nat -A OUTPUT -p tcp -m owner ! --uid-owner mitmproxyuser --dport 80 -j REDIRECT --to-port $port
     sudo ip6tables -t nat -A OUTPUT -p tcp -m owner ! --uid-owner mitmproxyuser --dport 443 -j REDIRECT --to-port $port
     
+    
     echo "Start and log traffic to $location/traffic.log";
     sudo mkdir -p $location
     sudo groupadd mitm-reports
@@ -46,17 +66,32 @@ then
     sudo chgrp -R mitm-reports $location
     sudo chmod -R 2775 $location
     
-    #export http_proxy=http://127.0.0.1:8080
-    #export https_proxy=https://127.0.0.1:8080
     
     sudo -u mitmproxyuser -H bash -c '/usr/bin/mitmproxy --mode transparent --showhost --set block_global=false -w /usr/share/mitm-data/traffic.log'
-else
-    echo "Setting all traffic from 8080. No local traffic."
-    sudo iptables -t nat -A PREROUTING -i wlp0s20f3 -p tcp --dport 80 -j REDIRECT --to-port $port
-    sudo iptables -t nat -A PREROUTING -i wlp0s20f3 -p tcp --dport 443 -j REDIRECT --to-port $port
+fi
+
+if [ "$local" = "-r" ]; # check if is via router
+then
+    echo "Setting all proxied router traffic for port 8080."
+    sudo iptables -t nat -A PREROUTING -i wlp0s20f3 -p udp --dport 80 -j REDIRECT --to-port $port
+    sudo iptables -t nat -A PREROUTING -i wlp0s20f3 -p udp --dport 443 -j REDIRECT --to-port $port
     sudo ip6tables -t nat -A PREROUTING -i wlp0s20f3 -p tcp --dport 80 -j REDIRECT --to-port $port
     sudo ip6tables -t nat -A PREROUTING -i wlp0s20f3 -p tcp --dport 443 -j REDIRECT --to-port $port
+    mitmproxy --mode transparent --showhost --set block_global=false -w ~/traffic.log
 fi
+
+if [ "$local" = "-w" ]; # check if is local waydroid vm, simple
+then
+    echo "Waydroid VM traffic routed to port 8080"
+    sudo iptables -t nat -A PREROUTING -i waydroid0 -p tcp --dport 80 -j REDIRECT --to-port $port
+    sudo iptables -t nat -A PREROUTING -i waydroid0 -p tcp --dport 443 -j REDIRECT --to-port $port
+    sudo ip6tables -t nat -A PREROUTING -i waydroid0 -p tcp --dport 80 -j REDIRECT --to-port $port
+    sudo ip6tables -t nat -A PREROUTING -i waydroid0 -p tcp --dport 443 -j REDIRECT --to-port $port
+
+
+    mitmproxy --mode transparent --showhost --set block_global=false -w ~/traffic.log
+fi
+
 
 echo "Setting ports 80, 443 to redirect to $port. Finished"
 sudo iptables -t nat -F
